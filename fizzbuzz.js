bf379d2a8f09d94ca1616b1d30753f994134ad91
Ryan Roat <ryanroat@gmail.com>
2020-02-15 11:10:39 -0500

    layout fixed by eslint

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,22 +1,22 @@
 function fizzbuzz(end) {
-  let i = 0;
-  let fizz, buzz = false;
-  for (i = 1; i <= end; i++) {
-    fizz = i % 3;
-    buzz = i % 5;
-    if ((!fizz) && (!buzz)) {
-      document.writeln(`fizz-buzz`);
+    let i = 0;
+    // eslint-disable-next-line one-var
+    let fizz,
+        buzz = false;
+    // eslint-disable-next-line no-plusplus
+    for (i = 1; i <= end; i++) {
+        fizz = i % 3;
+        buzz = i % 5;
+        if (!fizz && !buzz) {
+            document.writeln(`fizz-buzz`);
+        } else if (!fizz) {
+            document.writeln(`fizz`);
+        } else if (!buzz) {
+            document.writeln(`buzz`);
+        } else {
+            document.writeln(i);
+        }
     }
-    else if (!fizz) {
-      document.writeln(`fizz`);
-    }
-    else if (!buzz) {
-      document.writeln(`buzz`);
-    }
-    else {
-      document.writeln(i);
-    }
-  }
 }
 
 fizzbuzz(100);
